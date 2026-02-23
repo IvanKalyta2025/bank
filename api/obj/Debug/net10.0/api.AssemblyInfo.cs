18f814a39926ed529b7dbb354d1d9db2fe80ee14
IvanKalyta2025 <ivan25sfj@gmail.com>
2026-02-23 13:17:11 +0100

    Add: DockerFile Det fungerer,men gir en feilmelding

--- api/obj/Debug/net10.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net10.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a9857c673c61e2f5a02a84bc3adc2d4706aa39c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05de410a3829668da2208b50a8af0084390cb3a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

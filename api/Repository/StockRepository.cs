d1dcc787bd00511fb5a91806684660a128a2f117
IvanKalyta2025 <ivan25sfj@gmail.com>
2026-02-26 10:07:36 +0100

    Add StockRepository

--- api/Repository/StockRepository.cs
+++ api/Repository/StockRepository.cs
@@ -1,17 +1,21 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using api.Interfaces;
 using api.Models;
+using api.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace api.Repository
 {
     public class StockRepository : IStockRepository
     {
+        private readonly ApplicationDBContext _context;
+
+        public StockRepository(ApplicationDBContext context)
+        {
+            _context = context;
+        }
         public Task<List<Stock>> GetAllAsync()
         {
-            throw new NotImplementedException();
+            return _context.Stocks.ToListAsync();
         }
     }
 }

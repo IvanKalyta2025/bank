3522b35012098c9fad19a812a6e94a40e175003b
IvanKalyta2025 <ivan25sfj@gmail.com>
2026-02-26 14:18:35 +0100

    Add StockRepository

--- api/Repository/StockRepository.cs
+++ api/Repository/StockRepository.cs
@@ -2,6 +2,11 @@ using api.Interfaces;
 using api.Models;
 using api.Data;
 using Microsoft.EntityFrameworkCore;
+using System.Runtime.CompilerServices;
+using api.Dtos.Stock;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http.HttpResults;
+using api.Mappers;
 
 namespace api.Repository
 {
@@ -13,9 +18,36 @@ namespace api.Repository
         {
             _context = context;
         }
-        public Task<List<Stock>> GetAllAsync()
+        public async Task<Stock> CreateAsync(Stock stock)
         {
-            return _context.Stocks.ToListAsync();
+            await _context.Stocks.AddAsync(stock);
+            await _context.SaveChangesAsync();
+
+            return stock;
+        }
+        public async Task<List<Stock>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+        public async Task<Stock?> GetByIdAsync(int id)
+        {
+            var stock = await _context.Stocks.FindAsync(id);
+            return stock;
+        }
+        public async Task<Stock?> UpdateAsync(int id, UpdateStockRequestDto updateStockRequestDto)
+        {
+            throw new NotImplementedException();
+        }
+        public async Task<Stock?> DeleteAsync(int id)
+        {
+            var stockModel = await _context.Stocks.FirstOrDefaultAsync(x => x.Id == id);
+            if (stockModel == null)
+            {
+                return null;
+            }
+            _context.Remove(stockModel);
+            await _context.SaveChangesAsync();
+            return null;
         }
     }
 }
